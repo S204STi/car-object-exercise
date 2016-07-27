ce5b16a234f748f585eb46e5460bb47ccb106022
Roy <rechase84@gmail.com>
2016-07-27 10:04:30 -0600

    wip

--- car.js
+++ car.js
@@ -1,9 +1,25 @@
-
+'use strict';
 
 var Car = function (mpg) {
+	this.fuel = this.fill();
+	this.mpg = mpg;
+	this.trips = [];
+	this.odometer = 0;
+	this.drive = function(distance){
+		this.distance = distance;
+		this.gallons = this.gallons - this.distance / this.mpg;
+		this.trips.push(this.distance + " miles");
+		console.log(this.trips);
+	};
 };
 
 Car.prototype.fill = function (gallons) {
+	this.gallons = gallons;
 };
+
+Car.prototype.trip = function(distance){
+		this.odometer = distance;
+};
+
 
 module.exports = Car;
